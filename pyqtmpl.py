a19b8efa13fc1c585a691798867ddcf62d2dea08
David Eldon <eldond@fusion.gat.com>
2018-04-17 12:21:43 -0700

    Wrappers for figure, axes, and subplots

--- pyqtmpl.py
+++ pyqtmpl.py
@@ -4,6 +4,7 @@
 # Basic imports
 from __future__ import print_function, division
 import unittest
+import sys
 
 # Calculation imports
 import numpy as np
@@ -155,6 +156,92 @@ def plotkw_translator(**plotkw):
     return pgkw
 
 
+def figure(*args, **kwargs):
+    return Figure(*args, **kwargs)
+
+
+class Figure:
+    def __init__(
+            self, figsize=None, dpi=None,
+            facecolor=None, edgecolor=None, linewidth=0.0,
+            frameon=None, subplotpars=None, tight_layout=None, constrained_layout=None,
+    ):
+        pg.setConfigOption('background', 'w' if facecolor is None else facecolor)
+        pg.setConfigOption('foreground', 'k')
+        self.win = pg.GraphicsWindow()
+        if figsize is None:
+            figsize = (800, 600)
+        self.win.resize(figsize[0], figsize[1])
+        self.axes = None
+        self.layout = pg.GraphicsLayout()
+        self.win.setCentralItem(self.layout)
+        self.__call__()
+
+    def __call__(self):
+        print('call figure')
+        return self
+
+    def add_subplot(self, nrows, ncols, index, projection=None, polar=None, **kwargs):
+
+        row = int(np.floor(index/ncols))
+        if row > (nrows-1):
+            raise ValueError('index {} would be on row {}, but the last row is {}!'.format(index, row, nrows-1))
+        col = index % ncols
+        # ax = self.layout.addPlot(row, col)
+        ax = axes(self, row, col)
+        return ax
+
+
+class Axes:
+    def __init__(self, fig=None, *args, **kwargs):
+        if fig is None:
+            fig = figure()
+        self.fig = fig
+        self.axes = self.fig.layout.addPlot(*args, **kwargs)
+        self.__call__()
+
+    def plot(self, *args, **kwargs):
+        self.axes.plot(*args, **kwargs)
+
+    def __call__(self):
+        return self
+
+
+def axes(*args, **kwargs):
+    return Axes(*args, **kwargs)
+
+
+def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True, subplot_kw=None, gridspec_kw=None, **fig_kw):
+
+    def pick_share(share, ii, jj, axs_):
+        if ii == 0 and jj == 0:
+            return None
+        if share in ['all', True]:
+            return axs_[0, 0].axes
+        elif share in ['col']:
+            return axs_[0, jj].axes
+        elif share in ['row']:
+            return axs_[ii, 0].axes
+        else:
+            return None
+
+    fig = figure(**fig_kw)
+    axs = np.zeros((nrows, ncols), object)
+    subplot_kw = subplot_kw if subplot_kw is not None else {}
+    for i in range(nrows):
+        for j in range(ncols):
+            index = i*ncols + j
+            x_share_from = pick_share(sharex, i, j, axs)
+            y_share_from = pick_share(sharey, i, j, axs)
+            print('index {}, row {}, col {}, xshare = {}, yshare = {}'.format(index, i, j, x_share_from, y_share_from))
+            axs[i, j] = fig.add_subplot(nrows, ncols, index, **subplot_kw)
+            if x_share_from is not None:
+                axs[i, j].axes.setXLink(x_share_from)
+            if y_share_from is not None:
+                axs[i, j].axes.setYLink(y_share_from)
+    return fig, axs
+
+
 class TestPyQtMpl(unittest.TestCase):
     """
     Test from the command line with
@@ -215,6 +302,18 @@ class TestPyQtMpl(unittest.TestCase):
             newk[i] = plotkw_translator(**self.plot_kw_tests[i])
         if self.verbose:
             print('New keyword dictionaries:', newk)
+
+    def test_figure(self):
+        fig = figure()
+        if self.verbose:
+            print('test_figure: fig = {}'.format(fig))
+
+    def test_subplots(self):
+        x = np.linspace(0, 1.2, 20)
+        y = x**2 + 1
+        fig, axs = subplots(3, 2, sharex=True)
+        axs[1, 1].plot(x, y)
+
 
 if __name__ == '__main__':
     unittest.main()
