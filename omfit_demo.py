caebb247614e54128abeeec823ec19cfab64c907
David Eldon <eldond@fusion.gat.com>
2018-04-25 12:21:15 -0700

    Add omfit_demo.py, which has problems with blocking but kinda words
    - mention #24

---
+++ omfit_demo.py
@@ -0,0 +1,25 @@
+# -*-Python-*-
+# Created by eldond at 2018 Apr 25  11:55
+
+"""
+This script demonstrates pyqtmpl usage within OMFIT.
+
+Load this thing into the OMFIT by executing the following in the OMFIT command box (edit the path):
+
+    OMFIT['mpl_demo'] = OMFITpythonTask('/home/eldond/python_stuff/pyqtmpl/omfit_demo.py', modifyOriginal=True)
+"""
+
+from __future__ import print_function, division
+import pyqtmpl as mpl
+import pyqtmpl.pyplot as plt
+
+print(mpl.__version__)
+mpl.set_debug(True)
+
+x = linspace(0, 10, 101)
+y = sin(x*pi)
+
+plt.plot(x, y)
+scratch['qtapp'] = mpl.app
+
+mpl.app.exec_()
