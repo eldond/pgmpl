b3ac876e0399ac73b1e149798ffb970cddb41a1b
David Eldon <eldond@fusion.gat.com>
2018-05-30 09:00:59 -0700

    Move Legend.__call__ checks to new helper function

--- pgmpl/axes.py
+++ pgmpl/axes.py
@@ -622,6 +622,12 @@ class Legend:
             if self.supported(handle):
                 self.leg.addItem(handle, label)
 
+        self.check_call_kw(**kw)
+
+        return self
+
+    def check_call_kw(self, **kw):
+        """Checks keywords passed to Legend.__call__ and warns about unsupported ones"""
         unhandled_kws = dict(loc=None,
             numpoints=None,    # the number of points in the legend line
             markerscale=None,  # the relative size of legend markers vs. original
@@ -658,8 +664,6 @@ class Legend:
                     'pgmpl.axes.Legend.__call__ got unhandled keyword: {}. This keyword might be implemented later.')
         if len(kw.keys()):
             warnings.warn('pgmpl.axes.Legend.__call__ got unrecognized keywords: {}'.format(kw.keys()))
-
-        return self
 
     def addItem(self, item, name=None):
         """
