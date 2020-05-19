21d941e6d58d1cd5cd5c734a3a7da122897d2ac0
David Eldon <eldond@fusion.gat.com>
2020-05-19 15:19:30 -0700

    Version 0.4.0

--- pgmpl/info.py
+++ pgmpl/info.py
@@ -9,7 +9,7 @@ installation. So, this information is placed here and imported into __init__.py 
 """
 
 # Define module
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __maintainer__ = "David Eldon"
 __email__ = "eldond@fusion.gat.com"
 __status__ = "Development"
